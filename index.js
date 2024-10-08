56ef7aaaaf4912858a287842699ae4325a9b3730
Ian Kabaka <iankabaka1@gmail.com>
2024-10-08 12:31:01 +0300

    fetch api and converts  json

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      renderBooks(json);
+      // return json.forEach((pages) => console.log(pages.numberOfPages));
+    });
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
